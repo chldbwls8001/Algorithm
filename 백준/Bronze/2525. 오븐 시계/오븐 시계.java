53c8bc56cd6c226a878c5c2b0c599ddf1a201437
YUJIN CHOI <128793128+chldbwls8001@users.noreply.github.com>
2024-01-10 01:53:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 228 ms, Memory: 18488 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int time = sc.nextInt();
+        sc.close();
+
+        h += (m + time) / 60; 
+        m = (m + time) % 60;
+        if (h > 23){
+            h -= 24;
+        }
+        
+        System.out.println(h + " " + m);
+    }
+}
