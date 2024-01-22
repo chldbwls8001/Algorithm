2879485a8f60569e0290b05f64d0b444698a093b
YUJIN CHOI <128793128+chldbwls8001@users.noreply.github.com>
2024-01-22 20:46:38 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 196 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {    	
+    	Scanner sc = new Scanner(System.in);
+    	String str = sc.next();
+    	sc.close();
+    	
+    	int cnt = 0;
+    	
+    	for (int i = 0; i < str.length(); i++) {
+    		char ch = str.charAt(i);
+    		
+    		if (ch == 'c') {
+    			if (i < str.length() - 1) {
+    				if (str.charAt(i + 1) == '=') {
+    					i++;
+    				}
+    				else if (str.charAt(i + 1) == '-') {
+    					i++;
+    				}
+    			}
+    		}
+    		
+    		if (ch == 'd') {
+    			if (i < str.length() - 1) {
+    				if (str.charAt(i + 1) == 'z') {
+    					if (i < str.length() - 2 && str.charAt(i + 2) == '=') {
+    						i += 2;
+    					}
+    				}
+    				else if (str.charAt(i + 1) == '-') {
+    					i++;
+    				}
+    			}
+    		}
+    		
+    		if (ch == 'l') {
+    			if (i < str.length() - 1) {
+    				if (str.charAt(i + 1) == 'j') {
+    					i++;
+    				}
+    			}
+    		}
+    		
+    		if (ch == 'n') {
+    			if (i < str.length() - 1) {
+    				if (str.charAt(i + 1) == 'j') {
+    					i++;
+    				}
+    			}
+    		}
+    		
+    		if (ch == 's') {
+    			if (i < str.length() - 1) {
+    				if (str.charAt(i + 1) == '=') {
+    					i++;
+    				}
+    			}
+    		}
+    		
+    		if (ch == 'z') {
+    			if (i < str.length() - 1) {
+    				if (str.charAt(i + 1) == '=') {
+    					i++;
+    				}
+    			}
+    		}
+    		
+    		cnt++;
+    	}
+
+    	System.out.println(cnt);
+    }
+}
