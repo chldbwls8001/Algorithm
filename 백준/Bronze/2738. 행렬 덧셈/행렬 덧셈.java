7abcc720da7fd0b64e4d094c6b6dfe28a52b6ca7
YUJIN CHOI <128793128+chldbwls8001@users.noreply.github.com>
2024-01-23 00:44:10 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 680 ms, Memory: 36116 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {	
+    public static void main(String[] args) {    	
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	int M = sc.nextInt();
+    	
+    	int[][] A = new int[N][M];
+    	int[][] B = new int[N][M];
+    	
+    	for (int i = 0; i < N; i++) {
+    		for (int j = 0; j < M; j++) {
+    			A[i][j] = sc.nextInt();
+    		}
+    	}
+    	
+    	for (int i = 0; i < N; i++) {
+    		for (int j = 0; j < M; j++) {
+    			B[i][j] = sc.nextInt();
+    		}
+    	}
+    	
+    	int[][] arr = new int [N][M];
+    	
+    	for (int i = 0; i < N; i++) {
+    		for (int j = 0; j < M; j++) {
+    			arr[i][j] = A[i][j] + B[i][j];
+        		System.out.print(arr[i][j] + " ");
+    		}
+    		System.out.println();
+    	}
+    }
+}
