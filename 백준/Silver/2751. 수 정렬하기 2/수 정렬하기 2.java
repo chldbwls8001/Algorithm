825f48b6ca18fc10f8a2212bd8f4b8641cca9175
YUJIN CHOI <128793128+chldbwls8001@users.noreply.github.com>
2024-01-27 01:26:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2768 ms, Memory: 334200 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {	
+    public static void main(String[] args) {    	
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	ArrayList<Integer> list = new ArrayList<>();
+    	
+    	for (int i = 0; i < N; i++) {
+    		list.add(sc.nextInt());
+    	}
+    	
+    	Collections.sort(list);
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+    	for (int value : list) {
+    		sb.append(value).append('\n');
+    	}
+    	
+    	System.out.println(sb);
+    }
+}
