567e4ffba79c02dc74f547c18cd4bcddbc4ceb49
YUJIN CHOI <128793128+chldbwls8001@users.noreply.github.com>
2024-02-02 00:44:01 +0900

    [Bronze II] Title: Hashing, Time: 204 ms, Memory: 17684 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,29 +1,37 @@
 import java.util.Scanner;
 
-public class Main {	
-    public static void main(String[] args) {    	
-    	Scanner sc = new Scanner(System.in);
-    	int L = sc.nextInt();
-    	String str = sc.next();
-    	int[] stringToInt = new int[L];
-    	
-    	for (int i = 0; i < str.length(); i++) {
-    		stringToInt[i] = str.charAt(i) - 'a' + 1;
-    	}
-    	
-    	int hashValue = stringToInt[0]; // 해시 초기값: stringToInt[0] x 31^0
-    	
-    	for (int i = 1; i < stringToInt.length; i++) {
-    		int multiple = 1; // 계수
-    		
-    		for (int j = 1; j <= i; j++) {
-    			multiple *= 31;
-    		}
-    		
-    		hashValue += stringToInt[i] * multiple;
-    	}
-    	
-    	System.out.println(hashValue);
-    	sc.close();
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        String str = sc.next();
+
+        // 해시 함수의 상수 값
+        int r = 31;
+        int M = 1234567891;
+
+        long hashValue = calculateHash(str, r, M);
+
+        System.out.println(hashValue);
+        sc.close();
+    }
+
+    static long calculateHash(String str, int r, int M) {
+        int len = str.length();
+        long hashValue = 0;
+        long rPow = 1;
+
+        for (int i = 0; i < len; i++) {
+            // 각 알파벳에 해당하는 숫자를 가져옴
+            int ai = str.charAt(i) - 'a' + 1;
+
+            // 각 항에 대해 ai * r^i를 계산하고 해시값에 더함
+            hashValue = (hashValue + ai * rPow) % M;
+
+            // r^i를 계산하여 다음 항에 사용
+            rPow = (rPow * r) % M;
+        }
+
+        return hashValue;
     }
 }
