5e808dc179e420b82a39eaeaa9a3fc16a6ae311d
YUJIN CHOI <128793128+chldbwls8001@users.noreply.github.com>
2024-02-02 22:03:24 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt(); // int로 입력 받을 시 오버플로우 발생 가능(1 ≤ n ≤ 500,000)
+
+        System.out.println(n * (n - 1) / 2);
+        System.out.println(2);
+    }
+}
