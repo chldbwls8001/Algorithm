3a86c284b2df2f0719c8983a401f5f623c364cf2
YUJIN CHOI <128793128+chldbwls8001@users.noreply.github.com>
2024-02-15 19:33:48 +0900

    [Bronze II] Title: 대표값2, Time: 120 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze II/2587. 대표값2/대표값2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+        int tot = 0;
+
+        for (int i = 0; i < 5; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            tot += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(tot / 5); // 평균값
+        System.out.println(arr[2]); // 중앙값
+        br.close();
+    }
+}
